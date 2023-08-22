c1b66f2275dd1980aeb969a5811c83af16c18c1a
kalilmedfai <kalilmedfai@hotmail.fr>
2023-08-22 14:02:45 +0200

    utilisation méthode de l'objet array dans display-recipes.js

--- pages/display-recipes.js
+++ pages/display-recipes.js
@@ -1,17 +1,12 @@
 function AfficherRecettes() {
+  // Récupération de l'élément du DOM qui accueillera les fiches
+  const sectionRecipes = document.querySelector(".recipes-container");
 
   // Affiche les recettes
-  for (let i = 0; i < recipes.length; i++) {
-    
-    const article = recipes[i];
-    // Récupération de l'élément du DOM qui accueillera les fiches
-    const sectionRecipes = document.querySelector(".recipes-container");
-    
+  recipes.forEach(article => {
     const articleElement = document.createElement("article");
-    
     const divImage = document.createElement("div");
     divImage.setAttribute("class", "imgOfRecipe");
-
     const textContainer = document.createElement("div");
     textContainer.setAttribute("class", "textContainer");
 
@@ -25,7 +20,7 @@ function AfficherRecettes() {
 
     const div1Bis = document.createElement("div");
     div1Bis.setAttribute("class", "divOfTime");
-    
+
     const timerIcon = document.createElement("img");
     timerIcon.setAttribute("src", "assets/Logo/icons8-clock-144.png");
     timerIcon.style.height = "24px";
@@ -35,36 +30,38 @@ function AfficherRecettes() {
     timeElement.style.fontSize = "18px";
     timeElement.style.margin = "0";
     timeElement.innerHTML = `${article.time} min`;
-    
+
     const div2 = document.createElement("div");
     div2.setAttribute("class", "ingredientsAndDescription");
     const ingredientsElement = document.createElement("p");
     ingredientsElement.style.fontSize = "12px";
     ingredientsElement.setAttribute("class", "allIngredientsOfRecipe");
-    for (const ingredients of article.ingredients) {
-      const ingredientLine = ingredients.quantity ? `<b>${ingredients.ingredient}:</b> ${ingredients.quantity} ${ingredients.unit ?? ''}<br>` : `<b>${ingredients.ingredient}</b> ${ingredients.unit ?? ''}<br>`;
+
+    article.ingredients.forEach(ingredients => {
+      const ingredientLine = ingredients.quantity
+        ? `<b>${ingredients.ingredient}:</b> ${ingredients.quantity} ${ingredients.unit ?? ''}<br>`
+        : `<b>${ingredients.ingredient}</b> ${ingredients.unit ?? ''}<br>`;
       ingredientsElement.innerHTML += ingredientLine;
-    }
-      
+    });
+
     const descriptionElement = document.createElement("p");
     descriptionElement.style.fontSize = "12px";
-    descriptionElement.setAttribute("class", "descriptionOfRecipe")
-    descriptionElement.innerHTML = `${article.description}`
+    descriptionElement.setAttribute("class", "descriptionOfRecipe");
+    descriptionElement.innerHTML = `${article.description}`;
 
-    // On rattache la balise article a la section Fiches
+    // On rattache la balise article à la section Fiches
     sectionRecipes.appendChild(articleElement);
     articleElement.appendChild(divImage);
     articleElement.appendChild(textContainer);
     textContainer.appendChild(div1);
     div1.appendChild(nomElement);
-    div1.appendChild(div1Bis); 
+    div1.appendChild(div1Bis);
     div1Bis.appendChild(timerIcon);
     div1Bis.appendChild(timeElement);
     textContainer.appendChild(div2);
     div2.appendChild(ingredientsElement);
     div2.appendChild(descriptionElement);
-  }
-
+  });
 }
 
-AfficherRecettes()
+AfficherRecettes();
